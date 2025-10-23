360a156f54c978a2026636dcd6729ce70099ef6c
Pavroz <fyriones@yandex.ru>
2025-10-23 23:59:02 +0500

    autotests 23.10.2025(3)

--- tests/test_profiles.py
+++ tests/test_profiles.py
@@ -5,19 +5,23 @@ name_profile = 'autotest'
 new_name_profile = 'АВТОТЕСТОВОЕ НАИМЕНОВАНИЕ ПРОФИЛЯ'
 new_description_profile = 'АВТОТЕСТОВОЕ ОПИСАНИЕ'
 
+import pytest
+import os
+
+@pytest.mark.skipif(os.getenv('CI'), reason="Internal site not accessible from CI")
 def test_create_profile(auth, profiles_page):
     profiles_page.create_profile(name_profile)
     profiles_page.delete_profile(name_profile)
-
+@pytest.mark.skipif(os.getenv('CI'), reason="Internal site not accessible from CI")
 def test_delete_profile(auth, profiles_page):
     profiles_page.create_profile(name_profile)
     profiles_page.delete_profile(name_profile)
-
+@pytest.mark.skipif(os.getenv('CI'), reason="Internal site not accessible from CI")
 def test_edit_name_profile(auth, profiles_page):
     profiles_page.create_profile(name_profile)
     profiles_page.edit_name_profile(name_profile, new_name_profile)
     profiles_page.delete_profile(new_name_profile)
-
+@pytest.mark.skipif(os.getenv('CI'), reason="Internal site not accessible from CI")
 def test_edit_description_profile(auth, profiles_page):
     profiles_page.create_profile(name_profile)
     profiles_page.edit_description_profile(name_profile, new_description_profile)
