92f349fcb62f3315d268ed21158b0ef51b78890a
Pavroz <fyriones@yandex.ru>
2025-10-29 23:14:19 +0500

    Добавление функций рандомной генерации имени и описания профиля

--- pages/profiles_page.py
+++ pages/profiles_page.py
@@ -6,6 +6,8 @@ from selenium.webdriver.common.by import By
 from time import sleep
 from selenium.webdriver.support import expected_conditions as EC
 import allure
+import random
+import string
 
 class ProfilesPage(BasePage):
     page_url = '/profiles'
@@ -31,9 +33,23 @@ class ProfilesPage(BasePage):
                 print(f"Ошибка при получении названия карточки: {e}")
         return titles
 
+    @staticmethod
+    def generate_profile_name(prefix='test_', length = 20):
+        """Генерация имени для тестового профиля"""
+        suffix = ''.join(random.choice(string.ascii_lowercase + string.digits)
+                         for _ in range(length - len(prefix)))
+        return f'{prefix}{suffix}'
 
-    def create_profile(self, name, description=None):
+    @staticmethod
+    def generate_profile_description(prefix='TEST_', length = 50):
+        """Генерация описания для тестового профиля"""
+        suffix = ''.join(random.choice(string.ascii_lowercase + string.digits)
+                         for _ in range(length - len(prefix)))
+        return f'{prefix}{suffix}'
+
+    def create_profile(self, description=None):
         """Создает профиль и возвращает его имя"""
+        name = self.generate_profile_name()
         with allure.step('Создание профиля'):
             try:
                 if name in self.get_all_carts_titles():
@@ -62,11 +78,11 @@ class ProfilesPage(BasePage):
         """Удаляет профиль по имени"""
         # Находим и кликаем кнопку удаления для профиля с нужным именем
         with allure.step('Нажатие на кнопку удаления'):
-            sleep(1)
             delete_button = self.wait_for_presence(
                 (By.XPATH,
                  f'//*[contains(text(), "{profile_name}")]//ancestor::prominform-profile-card//span[@nztype="delete"]')
             )
+
             delete_button.click()
         with allure.step('Подтверждение удаления'):
             self.wait_for_clickable(loc.yes_button_from_delete).click()
@@ -77,7 +93,8 @@ class ProfilesPage(BasePage):
             )
 
 
-    def edit_name_profile(self, name_profile, new_name_profile):
+    def edit_name_profile(self, name_profile):
+        new_name_profile = self.generate_profile_name()
         # Поиск кнопки редактирования
         with allure.step('Нажатие на кнопку редактирования'):
             edit_button = self.wait_for_clickable(
@@ -86,16 +103,18 @@ class ProfilesPage(BasePage):
             )
             edit_button.click()
         with allure.step('Очистка и заполнение поля ввода "Наименование"'):
-            name_field = self.wait_for_visible(loc.name_field)
+            name_field = self.wait_for_clickable(loc.name_field)
             name_field.clear()
             name_field.send_keys(new_name_profile)
         with allure.step('Подтверждение редактирования'):
             self.wait_for_clickable(loc.apply_modals_button).click()
         with allure.step('Проверка, что наименование изменилось'):
             assert self.wait_for_presence((By.XPATH, f'//*[contains(text(), "{new_name_profile}")]'))
+        return new_name_profile
 
-
-    def edit_description_profile(self, name_profile, new_description_profile):
+    def edit_description_profile(self, name_profile):
+        new_description_profile = self.generate_profile_description()
+        sleep(1)
         # Поиск кнопки редактирования
         with allure.step('Нажатие на кнопку редактирования'):
             edit_button = self.wait_for_clickable(
@@ -111,8 +130,11 @@ class ProfilesPage(BasePage):
             self.wait_for_clickable(loc.apply_modals_button).click()
         with allure.step('Проверка, что описание изменилось'):
             assert self.wait_for_presence((By.XPATH, f'//*[contains(text(), "{name_profile}")]'))
+        return new_description_profile
 
-    def edit_full_profile(self, name_profile, new_name_profile, new_description_profile=None):
+    def edit_full_profile(self, name_profile):
+        new_name_profile = self.generate_profile_name()
+        new_description_profile = self.generate_profile_description()
         with allure.step('Нажатие на кнопку редактирования'):
             edit_button = self.wait_for_clickable(
                 (By.XPATH,
@@ -120,18 +142,21 @@ class ProfilesPage(BasePage):
             )
             edit_button.click()
         with allure.step('Очистка и заполнение полей ввода "Наименование" и "Описание профиля"'):
-            name_field = self.wait_for_visible(loc.name_field)
+            name_field = self.wait_for_clickable(loc.name_field)
             name_field.clear()
             name_field.send_keys(new_name_profile)
-            description_field = self.wait_for_visible(loc.description_field)
+            description_field = self.wait_for_clickable(loc.description_field)
             description_field.clear()
             description_field.send_keys(new_description_profile)
         with allure.step('Подтверждение редактирования'):
             self.wait_for_clickable(loc.apply_modals_button).click()
         with allure.step('Проверка, что наименование и описание изменились'):
             assert self.wait_for_presence((By.XPATH, f'//*[contains(text(), "{new_name_profile}")]'))
+        return new_name_profile
 
-    def copy_profile(self, name_profile, new_name_profile, new_description_profile=None):
+    def copy_profile(self, name_profile):
+        new_name_profile = self.generate_profile_name()
+        new_description_profile = self.generate_profile_description()
         with allure.step('Нажатие на кнопку копирования'):
             copy_button = self.wait_for_clickable(
                 (By.XPATH,
@@ -139,17 +164,17 @@ class ProfilesPage(BasePage):
             )
             copy_button.click()
         with allure.step('Очистка и заполнение полей ввода "Наименование" и "Описание профиля"'):
-            name_field = self.wait_for_visible(loc.name_field)
+            name_field = self.wait_for_clickable(loc.name_field)
             name_field.clear()
             name_field.send_keys(new_name_profile)
-            description_field = self.wait_for_visible(loc.description_field)
+            description_field = self.wait_for_clickable(loc.description_field)
             description_field.clear()
             description_field.send_keys(new_description_profile)
         with allure.step('Подтверждение копирования'):
             self.wait_for_clickable(loc.apply_modals_button).click()
         with allure.step('Проверка, что профиль успешно скопировался'):
             assert self.wait_for_presence((By.XPATH, f'//*[contains(text(), "{new_name_profile}")]'))
-
+        return new_name_profile
 
     def activate_profile(self, name_profile):
         activate_button = self.wait_for_clickable(loc.activate_profile_button)
--- tests/test_profiles.py
+++ tests/test_profiles.py
@@ -2,10 +2,11 @@ import allure
 import pytest
 
 
-name_profile = 'autotest'
-new_name_profile = 'АВТОТЕСТОВОЕ НАИМЕНОВАНИЕ ПРОФИЛЯ'
-new_description_profile = 'АВТОТЕСТОВОЕ ОПИСАНИЕ'
+# name_profile = 'autotest'
+# new_name_profile = 'АВТОТЕСТОВОЕ НАИМЕНОВАНИЕ ПРОФИЛЯ'
+# new_description_profile = 'АВТОТЕСТОВОЕ ОПИСАНИЕ'
 
+# ТЕСТЫ ПАРАЛЛЕЛИТЬ НА ДВА ОКНА
 
 @allure.feature('Страница профилей')
 class TestProfiles:
@@ -14,45 +15,46 @@ class TestProfiles:
     @allure.title('Проверка создания профиля')
     @pytest.mark.profiles
     def test_create_profile(self, auth, profiles_page):
-        profiles_page.create_profile(name_profile)
-        profiles_page.delete_profile(name_profile)
+        name = profiles_page.create_profile()
+        profiles_page.delete_profile(name)
 
     @allure.story('Позитивные сценарии')
     @allure.title('Проверка удаления профиля')
     @pytest.mark.profiles
     def test_delete_profile(self, auth, profiles_page):
-        profiles_page.create_profile(name_profile)
-        profiles_page.delete_profile(name_profile)
+        name = profiles_page.create_profile()
+        profiles_page.delete_profile(name)
 
     @allure.story('Позитивные сценарии')
     @allure.title('Проверка изменения имени профиля')
     @pytest.mark.profiles
     def test_edit_name_profile(self, auth, profiles_page):
-        profiles_page.create_profile(name_profile)
-        profiles_page.edit_name_profile(name_profile, new_name_profile)
+        name = profiles_page.create_profile()
+        new_name_profile = profiles_page.edit_name_profile(name)
         profiles_page.delete_profile(new_name_profile)
 
     @allure.story('Позитивные сценарии')
     @allure.title('Проверка изменения описания профиля')
     @pytest.mark.profiles
     def test_edit_description_profile(self, auth, profiles_page):
-        profiles_page.create_profile(name_profile)
-        profiles_page.edit_description_profile(name_profile, new_description_profile)
+        name = profiles_page.create_profile()
+        profiles_page.edit_description_profile(name)
         # sleep(2)
-        profiles_page.delete_profile(name_profile)
+        profiles_page.delete_profile(name)
 
     @allure.story('Позитивные сценарии')
     @allure.title('Проферка изменения имени и описания профиля')
     @pytest.mark.profiles
     def test_edit_full_profile(self, auth, profiles_page):
-        profiles_page.create_profile(name_profile)
-        profiles_page.edit_full_profile(name_profile, new_name_profile, new_description_profile)
+        name = profiles_page.create_profile()
+        new_name_profile = profiles_page.edit_full_profile(name)
         profiles_page.delete_profile(new_name_profile)
 
     @allure.story('Позитивные сценарии')
     @allure.title('Проферка копирования профиля')
     @pytest.mark.profiles
     def test_copy_profile(self, auth, profiles_page):
-        profiles_page.create_profile(name_profile)
-        profiles_page.edit_full_profile(name_profile, new_name_profile, new_description_profile)
+        name = profiles_page.create_profile()
+        new_name_profile = profiles_page.copy_profile(name)
+        profiles_page.delete_profile(name)
         profiles_page.delete_profile(new_name_profile)
